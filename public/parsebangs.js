fc192e6c2a5e4462a81b7a32c5ca8f5d64e446d3
Smitty <me@smitop.com>
2022-05-18 10:27:49 -0400

    don't error without bang

--- public/parsebangs.js
+++ public/parsebangs.js
@@ -3,9 +3,14 @@ const BANG_REGEX = /(!([a-z0-9]+([ \t\r\n\f\u00A0]|$)))/;
 function getBang(s) {
     s = s.toLowerCase();
     const matches = s.match(BANG_REGEX);
-    if (!matches) return null;
-    const bang = matches[0].trim().slice(1);
-    return bang;
+    if (!matches) return {
+        bangText: null,
+        removed: s.trim(),
+    };
+    return {
+        bangText: matches[0].trim().slice(1),
+        removed: s.replace(BANG_REGEX, "").trim(),
+    };
 }
 if (typeof window !== "undefined") {
     // running in main thread
@@ -18,13 +23,13 @@ if (typeof window !== "undefined") {
     const q = params.get("q");
     if (q) {
         const bangsDataPromise = fetch("bangs.json").then(res => res.json());
-        const bangText = getBang(q);
+        const { bangText, removed } = getBang(q);
         if (bangText) {
             (async () => {
                 const bangsData = await bangsDataPromise;
                 const bang = bangsData.filter(bang => bang.t === bangText)[0];
                 if (bang) {
-                    location.href = bang.u.replace(/{{{s}}}/g, q);
+                    location.href = bang.u.replace(/{{{s}}}/g, removed);
                 }
             })();
         }
--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
 importScripts("parsebangs.js");
 
 self.addEventListener("install", event => {
-    // TODO: caching
     self.skipWaiting(); // claim all clients immediately
 });
+
+// TODO cache this
+const bangsDataPromise = fetch("bangs.json").then(res => res.json());
+async function getBangData(bangText) {
+    return (await bangsDataPromise).filter(bang => bang.t === bangText)[0];
+}
 
 self.addEventListener("fetch", event => event.respondWith((async () => {
     const url = new URL(event.request.url);
     if (url.pathname === "/" || url.pathname === "/s" || url.pathname === "/s/") {
         const q = url.searchParams.get("q");
+        const defaul = url.searchParams.get("d");
         if (q) {
-            const bangsDataPromise = fetch("bangs.json").then(res => res.json());
-            const bangText = getBang(q);
-            if (bangText) {
-                const bangsData = await bangsDataPromise;
-                const bang = bangsData.filter(bang => bang.t === bangText)[0];
-                if (bang) {
-                    const redirUrl = bang.u.replace(/{{{s}}}/g, q);
-                    return new Response(redirUrl, {
-                        status: 302,
-                        statusText: "Moved Temporarily",
-                        headers: {
-                            "Location": redirUrl,
-                        }
-                    });
+            const { bangText, removed } = getBang(q);
+            let bang;
+            if (bangText) bang = await getBangData(bangText);
+            if (!bang && defaul) bang = await getBangData(defaul);
+            if (!bang) bang = await getBangData("duckduckgo");
+            const redirUrl = bang.u.replace(/{{{s}}}/g, removed);
+            return new Response(redirUrl, {
+                status: 302,
+                statusText: "Moved Temporarily",
+                headers: {
+                    "Location": redirUrl,
                 }
-            }
+            });
         }
         // TODO: serve normal homepage
     }

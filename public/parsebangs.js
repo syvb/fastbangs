14eb776021de64048c9bd13100e9ffc95c977528
Smitty <me@smitop.com>
2022-05-18 09:12:41 -0400

    non-sw impl

--- public/parsebangs.js
+++ public/parsebangs.js
@@ -1,1 +1,27 @@
 // SPDX-License-Identifier: Apache-2.0
+const BANG_REGEX = /(!([a-z0-9]+([ \t\r\n\f\u00A0]|$)))/;
+function getBang(s) {
+    s = s.toLowerCase();
+    const matches = s.match(BANG_REGEX);
+    if (!matches) return null;
+    const bang = matches[0].trim().slice(1);
+    return bang;
+}
+if (typeof window !== "undefined") {
+    // running in main thread
+    const params = new URLSearchParams(location.search);
+    const q = params.get("q");
+    if (q) {
+        const bangsDataPromise = fetch("bangs.json").then(res => res.json());
+        const bangText = getBang(q);
+        if (bangText) {
+            (async () => {
+                const bangsData = await bangsDataPromise;
+                const bang = bangsData.filter(bang => bang.t === bangText)[0];
+                if (bang) {
+                    location.href = bang.u.replace(/{{{s}}}/g, q);
+                }
+            })();
+        }
+    }
+}
